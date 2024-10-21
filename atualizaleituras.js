082d324b6c42c2aae72255d7c4c74dc681b187a0
WaterAlert-WA <wateralertwa@gmail.com>
2024-10-21 17:40:25 -0300

    Update atualizaleituras.js

--- atualizaleituras.js
+++ atualizaleituras.js
@@ -1,1 +1,15 @@
+    // Função JavaScript para atualizar o valor do H1 a cada 2 segundos
+    function atualizarLeitura() {
+      setInterval(function() {
+        // Faz uma requisição Ajax para buscar os dados do PHP
+        fetch('RecebeDados.php')
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById('leituraSensor').innerText = data + ' cm';
+          })
+          .catch(error => console.error('Erro ao buscar os dados:', error));
+      }, 1000); // Atualiza a cada 2 segundos
+    }
 
+    // Inicia a função assim que a página carregar
+    window.onload = atualizarLeitura;
